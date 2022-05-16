d50b0d974eb9c870e075b81359dc1d859f78fffd
bosun-further <constantgrower@gmail.com>
2022-05-16 18:07:22 +0800

    update go_call_java_example.go

--- src/main/golang/go_call_java_example.go
+++ src/main/golang/go_call_java_example.go
@@ -4,8 +4,9 @@ import (
     "os/exec"
 )
 string data_path = "/root/user_data1.csv"
+string jar_path = "p4p.jar"
 func main() {
-    cmd := exec.Command("java", "-jar", "p4p.jar", data_path)
+    cmd := exec.Command("java", "-jar",jar_path, data_path)
     if err := cmd.Run(); err != nil {
         log.Fatal(err)
     }
